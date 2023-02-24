9bbee335e4b455c8ff7918eae37d6be9cd84aac4
Marjon07 <126189100+Marjon07@users.noreply.github.com>
2023-02-24 01:16:17 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,16 @@
-const animationText = document.getElementById("animation-text");
+window.addEventListener("deviceorientation", handleOrientation, true);
 
-animationText.addEventListener("click", () => {
-  animationText.style.animation = "animate 2s ease-in-out infinite";
-});
+function handleOrientation(event) {
+  const animationText = document.getElementById("animation-text");
+
+  let x = event.beta;
+  let y = event.gamma;
+
+  if (x > 90) { x = 90 };
+  if (x < -90) { x = -90 };
+
+  let newX = ((y / 90) * 50);
+  let newY = ((x / 90) * -50);
+
+  animationText.style.transform = `translate(-50%, -50%) rotateX(${x}deg) rotateY(${y}deg) translate(${newX}px, ${newY}px)`;
+}
