ada3eab3d637e5b53e6625c25346da5a0b586c3a
Marjon07 <126189100+Marjon07@users.noreply.github.com>
2023-02-24 01:21:37 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,16 +1,5 @@
-window.addEventListener("deviceorientation", handleOrientation, true);
+const speechBubble = document.getElementById("speech-bubble");
 
-function handleOrientation(event) {
-  const animationText = document.getElementById("animation-text");
-
-  let x = event.beta;
-  let y = event.gamma;
-
-  if (x > 90) { x = 90 };
-  if (x < -90) { x = -90 };
-
-  let newX = ((y / 90) * 50);
-  let newY = ((x / 90) * -50);
-
-  animationText.style.transform = `translate(-50%, -50%) rotateX(${x}deg) rotateY(${y}deg) translate(${newX}px, ${newY}px)`;
-}
+setTimeout(function() {
+  speechBubble.classList.add("visible");
+}, 3000);
